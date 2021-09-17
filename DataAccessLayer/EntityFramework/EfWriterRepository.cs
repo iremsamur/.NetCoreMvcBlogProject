ce4006e4387c54f4b568264f02533c03f422b5ac
iremsamur <iremsamur129@gmail.com>
2021-09-17 19:00:23 +0300

    updated

--- DataAccessLayer/EntityFramework/EfWriterRepository.cs
+++ DataAccessLayer/EntityFramework/EfWriterRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EfWriterRepository  : GenericRepository<Writer>, IWriterDal
+    public class EfWriterRepository  : GenericRepository<Writer>, IWriterDal //Writer için
     {
     }
 }
