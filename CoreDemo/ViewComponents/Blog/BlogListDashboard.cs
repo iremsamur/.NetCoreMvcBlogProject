cdbdd5f6d64997942f6f74aa2e134ae66a766acf
iremsmr <iremsamur129@gmail.com>
2021-10-31 20:33:12 +0300

    project is updated

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+       
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            //Blogları kategorileriyle birlikte dashboaardda bize listeler
+            var values = blogManager.GetBlogListWithCategory();
+            return View(values); 
+        }
+    }
+}
