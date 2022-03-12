a5792702369fe0f524be86d1aae754f7facac3df
iremsmr <iremsamur129@gmail.com>
2022-03-12 20:39:29 +0300

    Web Api Layer is added

---
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        public async Task<IActionResult> Index()//apiye erişeceğimiz için
+                                                //buradaki metodu asenkronik async tanımlıyoruz. Sen asenkronik olarak çalışacaksın
+                                                //çünkü apilerle çalışıyoruz
+        {
+            var httpClient = new HttpClient();//httpclient'ı çağırıyorum
+            var responseMessage = await httpClient.GetAsync("https://localhost:44358/api/Default");
+            //verileri listelemek için httpclient'dan getasync kullanılır.
+            //istek göndereceğim web api adresini yazacağım. Bu url'i web apideki ilgili request url'dan aldım.
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);//burada deserializeobject kullanıyoruz.
+            //Bazılarında serialize bazılarında deserialize olur. Buraya Benmi employee'daki propertylerimi
+            //karşılamak için verileri tutacağım geçici bir Class1 sınıfını veriyorum.
+
+            return View(values);
+        }
+       //şimdi ekleme işlemi yazalım.
+       [HttpGet]
+       public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        //ek işlemi backend operasyonlarının yazılacağı apinin çağırılacağı metod
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Class1 p)
+        {
+
+            var httpClient = new HttpClient();//httpclient'ı çağırıyorum
+            //listeleme işleminde Deserialize kullanılırken eklemede serialize kullanılır
+            var jsonEmployee = JsonConvert.SerializeObject(p);//ekleyeceğim parametreyi serialize eder.
+            StringContent content = new StringContent(jsonEmployee,Encoding.UTF8,"application/json");//1.parametre contentin içertiği
+            //2. türü encoding türü, 3.türü mediatype
+            //türkçe karakter için UTF8 verdim. Ve apiden gelen veriler olduğu için türe json verdim
+            var responseMessage = await httpClient.PostAsync("https://localhost:44358/api/Default", content);
+            //ekleme işlemi içinde PostAsync kullanılır.
+            //Yani web api tarafı kodlarıın c#'a göre webde backendde yazıyorum
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //eğer başarılı ise
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+    }
+    public class Class1{
+        //Employee property'lerini değerlerini karşılayacak geçici sınıf
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ WebApi/Controllers/DefaultController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DataAccessLayer;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]//Bu rotasyon eğer biz bir api'yi bir web projesi üzerinde
+    //UI katmanında çağırmak istersek bu rotasyonu kullanıyoruz.
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        //listeleme işlemi
+        [HttpGet]//metodun üstüne türünün ne olduğunu belirtiyorum
+        //bu bir ekleme mi, silme mi ne ise yazıyorum.
+        //listeleme için HttpGet verdim.
+        public IActionResult EmpployeeList()
+        {
+            using var c = new Context();
+            var values = c.Employees.ToList();//employee listini bana getiriyor.
+            //bu mvc controller'u yani UI içinde yer alan controller olmadığı için view döndürmez.
+            return Ok(values);//başarılı olan isteklerin sonucunda dönecek olan Ok status kodu döner.
+
+        }
+        [HttpPost]//ekleme işlemi için HttpPost attribute'u actionresult üzerinde kullanılır.
+        public IActionResult AddEmployee(Employee employee)
+        {
+            using var c = new Context();
+            c.Add(employee);//yeni bir employee ekle
+            c.SaveChanges();//değişiklikleri veritabanına kaydet.
+            return Ok();
+        }
+        //id değerine göre listeleme
+        [HttpGet("{id}")]//bir parametre alsın ve bu parametreye göre listeleme yapması için
+        //parantez içinde parametre vererek get'i kullanırız.
+        public IActionResult EmployeeGet(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);//id'den gelen çalışanı bul
+            if (employee == null)
+            {
+                //listemde olmayan bir id varsa 
+                return NotFound();//bana hiçbir şey bulunamadı döndürsün. Yani işlem başarısız
+            }
+            else
+            {
+                //eğer o id'ye ait bir çalışan varsa Ok döndürsün. Yani işlem başarılı
+                return Ok(employee);
+            }
+        }
+        //veri silme
+        [HttpDelete("{id}")]//veri silme işleminde HttpDelete kullanılır.
+        //id'ye göre yani bir parametreye göre silmesi için parametre alır.
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);//id'den gelen silinecek çalışanı bul
+            if (employee == null)
+            {
+                //listemde olmayan bir id varsa 
+                return NotFound();//bana hiçbir şey bulunamadı döndürsün. Yani işlem başarısız
+            }
+            else
+            {
+                //eğer o id'ye ait bir çalışan varsa Ok döndürsün. Yani işlem başarılı
+                c.Remove(employee);// o çalışanı sil
+                c.SaveChanges();
+                return Ok(employee);
+            }
+        }
+        //veri güncelleme
+        [HttpPut]//veri güncellemede HttpPut attribute'unu kullanırız
+        public IActionResult EmployeeUpdate(Employee employee)//dışarıdan bir employee parametresi alıyor
+        {
+            using var c = new Context();
+            var emp = c.Find<Employee>(employee.ID);//id'den gelen silinecek çalışanı bul
+            if (emp == null)
+            {
+                //listemde olmayan bir çalışan varsa 
+                return NotFound();//bana hiçbir şey bulunamadı döndürsün. Yani işlem başarısız
+            }
+            else
+            {
+                //eğer o id'ye ait bir çalışan varsa Ok döndürsün. Yani işlem başarılı
+                emp.Name = employee.Name;//değeri güncellesin yani yeni değerini ata
+                c.Update(emp);//güncelle
+                c.SaveChanges();//değişiklikleri kaydet
+                return Ok(employee);
+            }
+        }
+
+
+    }
+}
